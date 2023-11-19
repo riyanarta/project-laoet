e5a4973041f7682d1055b4cdd3b2047fc884e82e
Riyan Arta <riyan.arta14@gmail.com>
2023-11-19 15:19:09 +0700

    parameters again

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/hello', (req, res) => {
 app.put('/edit/status/', cors() , async (req, res) => {
   const {status, nama, id} = req.body;
   const updateQuery = 'UPDATE test SET ? WHERE id = ?';
-  const updateParams = [{status, nama, id}, req.body.id];
+  const updateParams = [{status, nama, id}, id];
   db.query(updateQuery, updateParams, (error, result) => {
     if(error) {
       console.log(error);
