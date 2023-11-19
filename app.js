640f1c97b6ea125f8187d256653e786ccab0a279
Riyan Arta <riyan.arta14@gmail.com>
2023-11-19 11:49:26 +0700

    Adding new Routing

--- app.js
+++ app.js
@@ -4,29 +4,23 @@ const app = express();
 const db = require('./services/db');
 
 app.use(express.json());
-// app.use(cors(corsOptions));
-// app.use('/books', booksRouter);
-// app.get('/check', async (req, res) => {
-//     try {
-//       const { barcode } = req.body;
-//       const result = await db.any('SELECT * FROM biblio WHERE itemcode = $1', [barcode]);
-//       // console.log('Books:', books);
-//       res.json(result);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ error: 'Error retrieving books', details: err.message });
-//     }
-// });
 
-app.get('/check/:itemcode', cors(), async (req, res) => {
+app.get('/check/:itemcode', async (req, res) => {
   try {
-    // const { barcode } = req.body;
-    const result = await db.any('SELECT * FROM biblio WHERE itemcode = $1', [req.params.itemcode]);
-    // console.log('Books:', books);
-    res.json(result);
+    const itemcode = req.params.itemcode;
+    
+    // Query the MySQL database
+    db.query('SELECT * FROM biblio WHERE itemcode = ?', [itemcode], (error, results) => {
+      if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Error retrieving data', details: error.message });
+      } else {
+        res.json(results);
+      }
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Error retrieving books', details: err.message });
+    res.status(500).json({ error: 'Server error', details: err.message });
   }
 });
 
@@ -42,7 +36,7 @@ app.get('/checks/:title', cors(), async (req, res) => {
     res.status(500).json({ error: 'Error retrieving books', details: err.message });
   }
 });
-app.put('/checks/:itemcode', cors(), async (req, res) => {
+app.put('/edit/:itemcode', cors(), async (req, res) => {
   try {
     // const { barcode } = req.body;
     const {title} = req.params;
